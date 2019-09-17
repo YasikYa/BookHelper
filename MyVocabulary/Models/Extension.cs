9423b70a4bb57be2f85bf64c5de04e81a0d51399
Yaroslav Vodotyka <yaroslav.vodotyka@nure.ua>
2019-09-17 16:13:31 +0300

    Initial setup Identity, created domain classes

---
+++ MyVocabulary/Models/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyVocabulary.Models
+{
+    public class Extension
+    {
+        public int ExtensionId { get; set; }
+
+        public string ExtensionString { get; set; }
+
+        public virtual ICollection<File> Files { get; set; }
+    }
+}
---
+++ MyVocabulary/Models/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyVocabulary.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+
+        public string FileName { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ExtensionId { get; set; }
+
+        public virtual AppUser User { get; set; }
+
+        public virtual Extension Extension { get; set; }
+    }
+}
